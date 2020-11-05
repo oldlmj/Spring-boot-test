d632a9456202c288e6367e7b4f0b86e88525ca6d
mj <mengjia1230@gmail.com>
2020-11-05 15:44:26 +0800

    初始專案

---
+++ src/main/java/com/olmj/demo/SpringTest20201105Application.java
@@ -0,0 +1,13 @@
+package com.olmj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest20201105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest20201105Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/olmj/demo/SpringTest20201105ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olmj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest20201105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
